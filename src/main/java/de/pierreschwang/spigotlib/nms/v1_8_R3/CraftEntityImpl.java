e133e774a337bbfa350350b887431c1e818fc7b2
Pierre Maurice Schwang <mail@pschwang.eu>
2020-05-23 14:38:44 +0200

    removed nms imports and dependency

--- src/main/java/de/pierreschwang/spigotlib/nms/v1_8_R3/CraftEntityImpl.java
+++ src/main/java/de/pierreschwang/spigotlib/nms/v1_8_R3/CraftEntityImpl.java
@@ -2,18 +2,16 @@ package de.pierreschwang.spigotlib.nms.v1_8_R3;
 
 import de.pierreschwang.spigotlib.nms.CraftEntity;
 import de.pierreschwang.spigotlib.nms.NmsArmorStand;
-import net.minecraft.server.v1_8_R3.*;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class CraftEntityImpl implements CraftEntity {
 
     @Override
     public NmsArmorStand spawnHologramLine(Player viewer, Location location, String display) {
+        // TODO
         // Construct hologram
-        final WorldServer server = ((CraftWorld) location.getWorld()).getHandle();
+        /*final WorldServer server = ((CraftWorld) location.getWorld()).getHandle();
         final EntityArmorStand armorStand = new EntityArmorStand(server);
         armorStand.setGravity(false);
         armorStand.setLocation(location.getX(), location.getY(), location.getZ(), 0, 0);
@@ -26,7 +24,8 @@ public class CraftEntityImpl implements CraftEntity {
         return () -> {
             PacketPlayOutEntityDestroy entityDestroy = new PacketPlayOutEntityDestroy(armorStand.getId());
             ((CraftPlayer) viewer).getHandle().playerConnection.sendPacket(entityDestroy);
-        };
+        };*/
+        return null;
     }
 
 }
